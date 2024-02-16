10b788f0e3fed66c3f40dc17399d91d25ff669a2
kiran <kiransaidu179@gmail.com>
2024-02-16 15:37:11 +0530

    FirstCommit

---
+++ ProductManagement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppProduct
+{
+    internal class ProductManagement
+    {
+
+        static SortedList<int, Products1> listProduct = new SortedList<int, Products1>()
+       {
+           {1,new Products1(){PName="books",PPrice=23.4,PBrand="bbbb",MfDate=new DateTime(day:2,month:3,year:2023),ExpDate=new DateTime(day:2,month:4,year:2025) } },
+           {2,new Products1(){PName="Laptops",PPrice=80000.4,PBrand="llll",MfDate=new DateTime(day:3,month:3,year:2023),ExpDate=new DateTime(day:4,month:4,year:2034) } },
+            {3,new Products1(){PName="phonew",PPrice=30000.4,PBrand="",MfDate=new DateTime(day:1,month:1,year:2023),ExpDate=new DateTime(day:1,month:1,year:2029) } }
+       };
+        public SortedList<int, Products1> AllProducts()
+        {
+            return listProduct;
+        }
+    }
+}
---
+++ Products1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppProduct
+{
+    internal class Products1
+    {
+        public string? PName { get; set; }
+        public double PPrice { get; set; }
+        public string? PBrand { get; set; }
+        public DateTime MfDate { get; set; }
+        public DateTime ExpDate { get; set; }
+    }
+}
